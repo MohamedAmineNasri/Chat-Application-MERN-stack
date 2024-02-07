531e930dbd0333ad071d859f9c4cf3a11faaaaef
MohamedAmineNasri <mohamedamine.nasri@esprit.tn>
2024-02-07 21:03:43 +0100

    Connect frontend to backend APIs for user Login and Registration

--- Server/app.js
+++ Server/app.js
@@ -1,30 +1,36 @@
-    const express = require('express');
+const express = require("express");
 
-    const app = express();
+const app = express();
+var cors = require('cors')
 
-    app.use(express.json());
-    app.use(express.urlencoded({extended: true}));
 
-    //Bring in the models
-    require('./Models/Chatroom')
-    require('./Models/Message')
-    require('./Models/User')
-    
+//Setup Cors Origin
+app.use(cors()) 
 
-    //Bring in the routes 
-    app.use('/user',require("./Routes/user"))
-    app.use('/chatroom',require("./Routes/chatroom"))
-    
 
-   //Setup Error Handlers
-    const errorHandlers = require("./handlers/errorHandlers");
-    app.use(errorHandlers.notFound);
-    app.use(errorHandlers.mongoseErrors);
-    if (process.env.ENV === "DEVELOPMENT") {
-    app.use(errorHandlers.developmentErrors);
-    } else {
-    app.use(errorHandlers.productionErrors);
-    }
-    
 
-    module.exports = app;
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+//Bring in the models
+require("./Models/Chatroom");
+require("./Models/Message");
+require("./Models/User");
+
+//Bring in the routes
+app.use("/user", require("./Routes/user"));
+app.use("/chatroom", require("./Routes/chatroom"));
+
+
+
+//Setup Error Handlers
+const errorHandlers = require("./handlers/errorHandlers");
+app.use(errorHandlers.notFound);
+app.use(errorHandlers.mongoseErrors);
+if (process.env.ENV === "DEVELOPMENT") {
+  app.use(errorHandlers.developmentErrors);
+} else {
+  app.use(errorHandlers.productionErrors);
+}
+
+module.exports = app;
